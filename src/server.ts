87f15b1883ec5da1791080eda370e1736ce5af66
Didier Osindero <didierosindero@gmail.com>
2022-12-01 14:48:15 +0000

    add POST /updateQuizCompletes and handler

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,19 @@
-import express from "express";
+import express, { json } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import {
-  addDummyDbItems,
-  addDbItem,
-  getAllDbItems,
-  getDbItemById,
-  DbItem,
-  updateDbItemById,
+  // addDummyDbItems,
+  // addDbItem,
+  // getAllDbItems,
+  // getDbItemById,
+  // DbItem,
+  // updateDbItemById,
+  updateQuizCompletionTracker,
+  QuizCompletionTracker
+
 } from "./db";
 import filePath from "./filePath";
 
-// loading in some dummy items into the database
-// (comment out if desired, or change the number)
-addDummyDbItems(20);
 
 const app = express();
 
@@ -34,50 +34,61 @@ app.get("/", (req, res) => {
   res.sendFile(pathToFile);
 });
 
-// GET /items
-app.get("/items", (req, res) => {
-  const allSignatures = getAllDbItems();
-  res.status(200).json(allSignatures);
-});
+app.post("/updateQuizCompletes", (req, res) => {
+  const currentQuizMode = req.body.mode;
 
-// POST /items
-app.post<{}, {}, DbItem>("/items", (req, res) => {
-  // to be rigorous, ought to handle non-conforming request bodies
-  // ... but omitting this as a simplification
-  const postData = req.body;
-  const createdSignature = addDbItem(postData);
-  res.status(201).json(createdSignature);
-});
-
-// GET /items/:id
-app.get<{ id: string }>("/items/:id", (req, res) => {
-  const matchingSignature = getDbItemById(parseInt(req.params.id));
-  if (matchingSignature === "not found") {
-    res.status(404).json(matchingSignature);
+  if (currentQuizMode) {
+  updateQuizCompletionTracker(currentQuizMode);
+  res.status(200).json(QuizCompletionTracker)
   } else {
-    res.status(200).json(matchingSignature);
+    res.status(401).send('No body sent!')
   }
-});
+})
 
-// DELETE /items/:id
-app.delete<{ id: string }>("/items/:id", (req, res) => {
-  const matchingSignature = getDbItemById(parseInt(req.params.id));
-  if (matchingSignature === "not found") {
-    res.status(404).json(matchingSignature);
-  } else {
-    res.status(200).json(matchingSignature);
-  }
-});
+// // GET /items
+// app.get("/items", (req, res) => {
+//   const allSignatures = getAllDbItems();
+//   res.status(200).json(allSignatures);
+// });
 
-// PATCH /items/:id
-app.patch<{ id: string }, {}, Partial<DbItem>>("/items/:id", (req, res) => {
-  const matchingSignature = updateDbItemById(parseInt(req.params.id), req.body);
-  if (matchingSignature === "not found") {
-    res.status(404).json(matchingSignature);
-  } else {
-    res.status(200).json(matchingSignature);
-  }
-});
+// // POST /items
+// app.post<{}, {}, DbItem>("/items", (req, res) => {
+//   // to be rigorous, ought to handle non-conforming request bodies
+//   // ... but omitting this as a simplification
+//   const postData = req.body;
+//   const createdSignature = addDbItem(postData);
+//   res.status(201).json(createdSignature);
+// });
+
+// // GET /items/:id
+// app.get<{ id: string }>("/items/:id", (req, res) => {
+//   const matchingSignature = getDbItemById(parseInt(req.params.id));
+//   if (matchingSignature === "not found") {
+//     res.status(404).json(matchingSignature);
+//   } else {
+//     res.status(200).json(matchingSignature);
+//   }
+// });
+
+// // DELETE /items/:id
+// app.delete<{ id: string }>("/items/:id", (req, res) => {
+//   const matchingSignature = getDbItemById(parseInt(req.params.id));
+//   if (matchingSignature === "not found") {
+//     res.status(404).json(matchingSignature);
+//   } else {
+//     res.status(200).json(matchingSignature);
+//   }
+// });
+
+// // PATCH /items/:id
+// app.patch<{ id: string }, {}, Partial<DbItem>>("/items/:id", (req, res) => {
+//   const matchingSignature = updateDbItemById(parseInt(req.params.id), req.body);
+//   if (matchingSignature === "not found") {
+//     res.status(404).json(matchingSignature);
+//   } else {
+//     res.status(200).json(matchingSignature);
+//   }
+// });
 
 app.listen(PORT_NUMBER, () => {
   console.log(`Server is listening on port ${PORT_NUMBER}!`);
